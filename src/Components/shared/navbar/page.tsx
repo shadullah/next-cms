d1dcd9011091404aa5fef3ea4a3a4d238382e276
hexaalif <shadullahsad9@gmail.com>
2025-01-06 23:40:03 +0600

    project setup

---
+++ src/Components/shared/navbar/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <h1>/||/\/</h1>
+        <ul>
+          <li>Dashboard</li>
+          <li>Get in touch</li>
+          <li>Login</li>
+          <li>=</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
